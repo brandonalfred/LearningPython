d1d55925eadaae18bdd5aa72f40961178b7aa115
Brandon Alfred <bca25@txstate.edu>
2018-03-25 15:59:11 -0500

    basic hello world

---
+++ hello.py
@@ -0,0 +1,15 @@
+print("\nHello World!")
+
+name = "Brandon Alfred"
+
+print("\nHello everyone, my name is " + name)
+
+name = 21
+
+print(name)  # variable types can be changed dynamically
+
+# this is a comment
+
+'''
+this is how you make a comment blocks
+'''
